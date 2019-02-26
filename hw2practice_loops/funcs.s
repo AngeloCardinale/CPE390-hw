71dddb3a9ec6c29d7d1e32fcc8ee6ba251f5b318
AngeloCardinale <Angelo.A.Cardinale@gmail.com>
2019-02-25 22:19:00 -0500

    HW2 fixed

--- hw2practice_loops/funcs.s
+++ hw2practice_loops/funcs.s
@@ -4,48 +4,48 @@
 
 	.globl _Z5counti
 _Z5counti:               
-	PUSH R0             
-        LDR R0, #0
-.l1
+	            
+        MOV R0, #0
+.l1:
 	ADD R0, #1
 	CMP R0, #10
 	BLT .l1
-	POP R0	
+		
 
 	BX LR
 	
 	.globl _Z9countDowni
 _Z9countDowni:
-        PUSH R0
-        LDR R0, #10
-.l2
+        
+        MOV R0, #10
+.l2:
 	SUB R0, #1
-	BLZ .l2
-	POP R0
+	CMP R0, #0
+	BLT .l2
+	
 
 	BX LR
 	
 	.globl _Z3sumii
 _Z3sumii:
-        PUSH R0, R1, R2, R3
+        PUSH {R2, R3}
         MOV R2, R0 
 	MOV R3, R0
-.l3
+.l3:
 	ADD R3, #1
 	ADD R2, R3, R2
 	CMP R3, R1
 	BLT .l3
 	MOV R0, R2
 	
-	POP R0, R1, R2, R3
+	POP {R2, R3}
 
-	ret
 
 	BX LR
 	
 	.globl _Z4facti
 _Z4facti:
-	PUSH R0, R1
+	PUSH {R1}
         MOV R1, R0
 	CMP R0, #2
 	SUBGE R1, #1
@@ -54,15 +54,14 @@ _Z4facti:
 	MOVEQ R1, #1
 	CMP R0,#0
 	ADDEQ R0, #1
-.l4
+.l4:
 	MUL R0, R0, R1
 	CMP R1, #2
 	SUBGE R1, #1
 	BGE .l4
 
-	POP  R0, R1
+	POP {R1}
 	
-	ret
 	
 	BX LR
 
